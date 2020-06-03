32b8c4b636aff2eb99657aa1ed0c233c40ee4fb3
elventian <elventian@gmail.com>
2020-06-03 13:23:37 +0300

    Added map data, initial units, drakkar

--- cover.hx
+++ cover.hx
@@ -2,6 +2,7 @@
 // You can add what you want to the save
 savedInt 		= 0; // Here is the initialization for the first launch, after that the value will depend on what has been saved
 
+
 function saveState() {
 	state.scriptProps = {
 		savedInt 		: savedInt,
@@ -10,13 +11,28 @@ function saveState() {
 
 
 // --- Local variables ---
+var drakkarSent = false;
+var drakkarSpawnZone = 137;
 var prevDiscoveredZones:Array<Int> = [];
 var towerZones:Array<Int> = [];
 var prevTowerZones:Array<Int> = [];
 var scoutZones:Array<Int> = [];
 var scoutZonesTime:Array<Float> = [];
-var zones = [122, 107, 112];
-var neighbours:Array<Array<Int>> = [[107, 112, 127], [122, 112, 100], [107, 122, 127, 111, 91, 100, 121]];
+var zones = [122, 107, 112, 127, 136, 121, 93, 64, 100, 86, 72, 57, 111, 91, 79, 69, 49, 128, 113, 101, 87, 76, 61, 134, 124, 110, 97, 82, 63,
+54, 130, 117, 102, 88, 75, 59, 106, 94, 77, 85];
+                                     //122              107                   112                     127         136        121
+var neighbours:Array<Array<Int>> = [[107,112,127], [122,112,100], [107,122,127,111,91,100,121], [122,112,136], [127,121], [112,111,113,128],
+// 93                64     100                    86              72            57             111                         91
+[107,100,86,72,64], [57], [93,86,91,111,112,107], [93,100,91,79], [57,69,86,79], [64,72,69,49], [112,100,91,101,121,113], [100,86,79,87,101,111,112],
+//79              69                      49           128           113         101                        87                     76
+[86,72,69,76,91], [72,57,49,61,76,79,86], [57,69,61], [121,113,124], [121,110], [113,111,91,87,82,97,110], [91,79,76,82,97,101], [79,69,61,63,82,87],
+//61               134           124                         110                97                          82             63
+[49,69,76,63,54], [128,124,130], [128,113,110,117,130,134], [113,102,117,124], [101,87,82,88,102,110,113], [63,75,88,97], [61,54,59,75,82,76],
+//54            130    117                   102                 88                  75                59             106             94
+[61,59,49,63], [134], [130,124,110,102,106], [110,97,88,94,117], [82,75,77,94,102], [82,63,59,77,88], [54,63,75,77], [117,102,94,85], [102,88,77,85],
+//77            85
+[88,75,94,85], [94,77]];
+
 function getNeighbours(zoneId: Int) : Array<Int> {
 	for (i in 0...zones.length) {
 		if (zones[i] == zoneId) {
@@ -37,15 +53,38 @@ function onFirstLaunch() {
 	state.objectives.title = "Settle on island and find its secrets"; //TODO: why not displayed?
 	state.objectives.summary = "Settle on island and find its secrets";
 
-	state.objectives.add("Settle", "Colonize ::value:: zones with food sources");
-	state.objectives.setGoalVal("Settle", 2);
-	state.objectives.setCurrentVal("Settle", 0);
+	state.objectives.add("Lighthouse", "Build a lighthouse to help other ships find their way here");
+	state.objectives.add("Lost ship", "One of your ships went astray, send expedition to find where it moored");
 
-	state.objectives.add("Comrads", "Find lost ship of Delmutt");
+	addRule(Rule.VillagerStrike);
+	player.addResource(Resource.Food, 100);
+
+	spawnInitialUnits();
+	savedInt = 1;
 }
 
 function onEachLaunch() {
+	trace(drakkarSpawnZone);
+}
 
+function spawnInitialUnits() {
+	player.zones[0].addUnit(Unit.Warrior, 2, player);
+	player.zones[0].addUnit(Unit.AxeWielder, 1, player);
+	player.zones[0].addUnit(Unit.Sheep, 2, player);
+}
+
+function generateDrakkarUnits(): Array<UnitKind> {
+	var armySize = randomInt(6);
+	var axeNum = randomInt(3);
+	var civilNum = randomInt(3);
+	var sheepNum = randomInt(2) + 1;
+
+	var res:Array<UnitKind> = [];
+	for (i in 0...axeNum) { res.push(Unit.AxeWielder); }
+	for (i in 0...(armySize - axeNum)) { res.push(Unit.Warrior); }
+	for (i in 0...civilNum) { res.push(Unit.Villager); }
+	for (i in 0...sheepNum) { res.push(Unit.Wolf); }
+	return res;
 }
 
 function playerHasUnitsInZone(zone: Zone) {
@@ -64,11 +103,9 @@ function updateFog() {
 				scoutZones.indexOf(zone.id) == -1) {
 				scoutZones.push(zone.id);
 				scoutZonesTime.push(state.time);
-				trace(scoutZones);
 			}
 		}
 	}
-	trace(scoutZonesTime);
 
 	//discover new zones, opened by watch towers
 	towerZones = [];
@@ -146,4 +183,14 @@ function updateFog() {
 // Regular update is called every 0.5s
 function regularUpdate(dt : Float) {
 	updateFog();
+
+	var sheepsNum = 0;
+	for (unit in player.zones[0].units) {
+		if (unit.kind == Unit.Sheep) { sheepsNum++; }
+	}
+
+	if (state.time > 5 && !drakkarSent) {
+		drakkarSent = true;
+		//drakkar(player, player.zones[0], getZone(drakkarSpawnZone), 0, 0, generateDrakkarUnits());
+	}
 }
